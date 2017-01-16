dcb05abc3f2de9a5ae652efc83a13b2af0be9305
Swapnil Gaikwad <swapnilg@thoughtworks.com>
2017-01-16 15:58:36 +0530

    Changed the type of input button to submit to get called onSubmit and added values on input text.

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -11,15 +11,15 @@ class CommentForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        let author = this._authorInput;
-        let comment = this._bodyInput;
+        let author = this._authorInput.value;
+        let comment = this._bodyInput.value;
     }
 
     render() {
         return <form onSubmit={this.handleSubmit.bind(this)}>
             Author : <input type="text" placeholder="Author" ref={(input) => this._authorInput = input}/>
             Comment: <input type="text" placeholder="Comment" ref={(commentInput => this._bodyInput = commentInput)}/>
-            <input type="button" value="Submit"/>
+            <input type="submit" value="Submit"/>
         </form>;
     }
 }
